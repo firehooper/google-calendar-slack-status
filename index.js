3e12d4ea7bd2d7329308c947d87845201559915b
Sam Domonkos <sam@pfgltd.com>
2019-05-14 09:37:34 -0400

    use time zone for parsing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const slack = require('slack');
-const moment = require('moment');
+const moment = require('moment-timezone');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,13 +17,15 @@ app.post('/', (req, res, next) => {
   // check for secret token
   if (!req.body.token || req.body.token !== process.env.SECRET_TOKEN) next();
   console.log('handling request', req.body);
+  console.log('server time zone', moment.tz.guess());
   // grab status and clean it up
   let status = req.body.title;
   const dndToken = ' [DND]';
   // parse event start/stop time
   const dateFormat = 'MMM D, YYYY [at] hh:mmA';
-  const start = moment(req.body.start, dateFormat);
-  const end = moment(req.body.end, dateFormat);
+  const timeZone = 'US/Eastern';
+  const start = moment.tz(req.body.start, dateFormat, timeZone);
+  const end = moment.tz(req.body.end, dateFormat, timeZone);
   const end_unix = end.unix();
   // check for DND
   if (status.includes(dndToken)) {
@@ -46,36 +48,36 @@ app.post('/', (req, res, next) => {
   res.send('ok');
 });
 
-app.get('/', (req, res, next) => {
-  // welcome message
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <title>Welcome!</title>
-        <style>
-          pre {
-            background-color: #DDD;
-            padding: 1em;
-            display: inline-block;
-          }
-        </style>
-      </head>
-      <body>
-        <h1>Your Heroku server is running!</h1>
-        <p>You'll need the following information for your IFTTT recipe:</p>
-        <h3>Body</h3>
-<pre>{
-  "title":"<<<{{Title}}>>>",
-  "start":"{{Starts}}",
-  "end":"{{Ends}}",
-  "token": "${process.env.SECRET_TOKEN}"
-}</pre>
-      </body>
-    </html>
-  `);
-});
+// app.get('/', (req, res, next) => {
+//   // welcome message
+//   res.send(`
+//     <!DOCTYPE html>
+//     <html>
+//       <head>
+//         <meta charset="utf-8">
+//         <title>Welcome!</title>
+//         <style>
+//           pre {
+//             background-color: #DDD;
+//             padding: 1em;
+//             display: inline-block;
+//           }
+//         </style>
+//       </head>
+//       <body>
+//         <h1>Your Heroku server is running!</h1>
+//         <p>You'll need the following information for your IFTTT recipe:</p>
+//         <h3>Body</h3>
+// <pre>{
+//   "title":"<<<{{Title}}>>>",
+//   "start":"{{Starts}}",
+//   "end":"{{Ends}}",
+//   "token": "${process.env.SECRET_TOKEN}"
+// }</pre>
+//       </body>
+//     </html>
+//   `);
+// });
 
 app.use((req, res, next) => {
   res.status(404);
